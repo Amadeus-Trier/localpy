5406fb562f0fd7718b998c121f71e5e3aad62a3e
Wissam_Air <git@amadeus-werbung.de>
2022-06-21 20:59:58 +0200

    neu

---
+++ Scraping/Neuer Ordner/Image-Scraping-Python/main.py
@@ -0,0 +1,54 @@
+'''
+Author - Aditya Mangal
+Date - 27 october 2020
+Purpose - Python mini task
+'''
+
+import bs4
+import requests
+import shutil
+import os
+
+website = \
+    'https://www.tshirt24.net/'  
+  
+
+def extract(data, quantity):
+    URL_input = website + data
+    print('Fetching from url =', URL_input)
+    URLdata = requests.get(URL_input)
+    soup = bs4.BeautifulSoup(URLdata.text, "html.parser")
+    ImgTags = soup.find_all('img')
+    i = 0
+    print('Please wait..')
+    while i < quantity:
+
+        for link in ImgTags:
+            try:
+                images = link.get('src')
+                ext = images[images.rindex('.'):]
+                if ext.startswith('.img'):
+                    ext = '.png'
+                elif ext.startswith('.jpg'):
+                    ext = '.jpg'
+                elif ext.startswith('.jfif'):
+                    ext = '.jfif'
+                elif ext.startswith('.com'):
+                    ext = '.jpg'
+                elif ext.startswith('.svg'):
+                    ext = '.svg'
+                data = requests.get(images, stream=True)
+                filename = str(i) + ext
+                with open(filename, 'wb') as file:
+                    shutil.copyfileobj(data.raw, file)
+                i += 1
+            except:
+                pass
+    print('\t\t\t Download erfolgreich\t\t ')
+
+
+data = input('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nWas suchst du? ')
+quantity = int(input('HWieviele Daten benötigst du? '))
+extract(data, quantity)
+
+
